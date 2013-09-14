3f839d7b083b3057a1d2c3bae0f73b307f97ce92
Greg Misicko <gregmmail@gmail.com>
2013-09-14 09:55:35 -0400

    structuring document

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
+<!DOCTYPE html>
+
+<head>
+    <title>CS E-15 Project 1 : Greg Misicko</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+
+<body>
 Welcome to P1
+</body>
+
+</html>
