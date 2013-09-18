1531fdf5bc46f607d5163eccd642f07f8d944388
Greg Misicko <gregmmail@gmail.com>
2013-09-17 20:57:47 -0400

    added style sheet and content

--- index.php
+++ index.php
@@ -3,14 +3,20 @@
 <head>
     <title>CS E-15 Project 1 : Greg Misicko</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="http://p1.talen.me/misicko.css">
 </head>
 
 <body>
 <p>Greg Misicko</p>
 
-<p>Biography information</p>
+<p>Biography information: Went to the same high school that Steve Jobs had gone
+to. Got my BS in computer engineering, then moved to Hawaii for a while before
+coming to Boston for a better career. Currently work as a software dev manager
+in a large company. Also an endurance cyclist - my most recent accomplishment
+was <a href="http://www.londonedinburghlondon.com/">London-Edinburgh-London</a>.
+</p>
 
-<p>Development Environment</p>
+<p>Editor: gedit</p>
 </body>
 
 </html>
